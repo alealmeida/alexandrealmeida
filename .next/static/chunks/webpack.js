ba882793f972577dd77a283ea624c7f1ceda1c9a
alealmeida <ale.142@icloud.com>
2022-02-07 17:46:39 -0300

    estrutural width 100% (no calc)

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f30b9d253731f0d944a5"; }
+/******/ 		__webpack_require__.h = function() { return "858b1ca596991d43e1a8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
