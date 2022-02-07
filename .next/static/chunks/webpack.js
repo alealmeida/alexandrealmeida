dbd0e42018c93969aed2ce519610a6450f1510e3
alealmeida <ale.142@icloud.com>
2022-02-07 17:40:56 -0300

    responsivo estável

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a1ba281a49ad2e1ffc9a"; }
+/******/ 		__webpack_require__.h = function() { return "f30b9d253731f0d944a5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
