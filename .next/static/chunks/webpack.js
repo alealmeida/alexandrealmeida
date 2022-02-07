30e68f55622e53ebaa06adcdda40faced19abafd
alealmeida <ale.142@icloud.com>
2022-02-07 18:31:27 -0300

    acerto responsivo pag inicial

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "858b1ca596991d43e1a8"; }
+/******/ 		__webpack_require__.h = function() { return "aefc334579f50a5b98c1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
