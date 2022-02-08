8766d56b0d17e6c189bf73e572d31c8647125169
alealmeida <ale.142@icloud.com>
2022-02-08 12:44:03 -0300

    css responsivo pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "aefc334579f50a5b98c1"; }
+/******/ 		__webpack_require__.h = function() { return "4936da99f5955c371ed0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
