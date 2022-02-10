56ff87d8910e66ef2e931f88d9262592fd6ec475
alealmeida <ale.142@icloud.com>
2022-02-10 14:17:18 -0300

    GA + Width 100% (article) estável

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "58916acb7679e18ceaa9"; }
+/******/ 		__webpack_require__.h = function() { return "333ab1ebde53ff6b9b5d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
